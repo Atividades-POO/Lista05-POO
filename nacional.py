c9bf45c76e38c3afdc38e29c43c70a442e994341
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-08 17:55:36 -0300

    Update nacional.py

--- nacional.py
+++ nacional.py
@@ -16,3 +16,22 @@
 # normal;
 # e) Crie um programa exemplo para demonstrar a criação de
 # um carro de cada tipo.
+
+# impotar a classe Veiculo
+from veiculo import Veiculo # importar a classe Veiculo
+class VeiculoNacional(Veiculo): # criar a classe VeiculoNacional herdando da classe Veiculo
+    def __init__(self, placa, tipo, cor, nPortas, preco): # criar o construtor da classe VeiculoNacional herdando do construtor da classe Veiculo
+        super().__init__(placa, tipo, cor, nPortas) # chamar o construtor da classe Veiculo
+        self.preco = preco # definir o atributo preco com o valor passado como parâmetro
+
+    def __str__(self): # criar o método __str__ para retornar os dados do veículo separados por vírgula e imprimir
+      return  f"{super().__str__()} + ',' + str(self.preco)"
+
+    #getters e setters
+    @property
+    def preco(self):
+        return self._preco
+
+    @preco.setter
+    def preco(self, preco):
+        self._preco = preco
