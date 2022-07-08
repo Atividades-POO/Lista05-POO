ddabe7262d217e1bfd7a7b890daabff91182e156
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-08 17:51:12 -0300

    Update importado.py

--- importado.py
+++ importado.py
@@ -5,3 +5,14 @@
 # Carlos Eduardo
 # data: 08/07/2022
 #
+# a) Crie as classes “VeiculoNacional” e “VeiculoImportado”
+# herdando da classe “Veiculo” da questão anterior;
+# b) Adicione o atributo preço à classe Veiculo e o receba no
+# construtor das novas classes;
+# c) No caso do veículo importado, ao ser atribuído o preço (seja
+# pelo construtor ou pelo acesso à propriedade), adicione 30%
+# ao valor do veículo;
+# d) No caso do veículo nacional, deverá ser mantido o preço
+# normal;
+# e) Crie um programa exemplo para demonstrar a criação de
+# um carro de cada tipo. 
