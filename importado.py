cfe105d21b918be380579a748d4e78984e5912a2
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-08 18:04:25 -0300

    Update importado.py

--- importado.py
+++ importado.py
@@ -15,4 +15,23 @@
 # d) No caso do veículo nacional, deverá ser mantido o preço
 # normal;
 # e) Crie um programa exemplo para demonstrar a criação de
-# um carro de cada tipo. 
+# um carro de cada tipo.
+
+# impotar a classe Veiculo
+from veiculo import Veiculo # importar a classe Veiculo
+class VeiculoImportado(Veiculo): # criar a classe VeiculoImportado herdando da classe Veiculo
+    def __init__(self, placa, tipo, cor, nPortas, preco): # criar o construtor da classe VeiculoImportado herdando do construtor da classe Veiculo
+        super().__init__(placa, tipo, cor, nPortas) # chamar o construtor da classe Veiculo
+        self.preco = preco # definir o atributo preco com o valor passado como parâmetro
+
+    def __str__(self): # criar o método __str__ para retornar os dados do veículo separados por vírgula e imprimir
+      return  f"{super().__str__()}, R$ {str(self.preco)}"
+
+    #getters e setters
+    @property
+    def preco(self):
+        return self._preco
+
+    @preco.setter
+    def preco(self, preco):
+        self._preco = preco + (preco * 0.3) # adicionar 30% ao valor do veículo
