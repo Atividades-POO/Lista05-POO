cd059f553dc191d7ee9620e6c52afd824e02659c
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-08 18:33:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ print(carro4)
 
 #
 #############################################################################################################################
-print('#############################################')
-print("#            Exercício 3                    #")
-print("#############################################")
+# print('#############################################')
+# print("#            Exercício 3                    #")
+# print("#############################################")
 #
 # a) Altere o programa Main da questão anterior para
 # cadastrar vários veículos, recebendo os dados a partir
@@ -51,4 +51,55 @@ print("#############################################")
 # cadastrado a essa lista.
 # c) Percorra a lista de veículos e exiba o preço de cada
 # um.
+
+Menu = True
+listaVeiculos = []
+while Menu:
+    print('#############################################')
+    print("#            Exercício 3                    #")
+    print("#############################################")
+    print('#')
+    print('# 1 - Cadastrar veículo')
+    print('# 2 - Listar veículos')
+    print('# 3 - Sair')
+    print('#')
+    opcao = int(input('Digite a opção desejada: '))
+    if opcao == 1:
+        print('#')
+        print('# 1 - Veículo nacional')
+        print('# 2 - Veículo importado')
+        print('#')
+        fabricante = int(input('Digite o tipo do veículo: '))
+        if fabricante == 1:
+            placa = input('Digite a placa do veículo: ')
+            fabricante = input('Digite o tipo do veículo: ')
+            cor = input('Digite a cor do veículo: ')
+            nPortas = int(input('Digite o número de portas do veículo: '))
+            preco = int(input('Digite o preço do veículo: '))
+            listaVeiculos.append(VN(placa, fabricante, cor, nPortas, preco))
+        elif fabricante == 2:
+            placa = input('Digite a placa do veículo: ')
+            fabricante = input('Digite o tipo do veículo: ')
+            cor = input('Digite a cor do veículo: ')
+            nPortas = int(input('Digite o número de portas do veículo: '))
+            preco = int(input('Digite o preço do veículo: '))
+            listaVeiculos.append(VI(placa, fabricante, cor, nPortas, preco))
+        else:
+            print('Opção inválida')
+    elif opcao == 2:
+        print('#')
+        print('#')
+        for i in listaVeiculos:
+            print(i)
+        print('#')
+        print('#')
+    elif opcao == 3:
+        Menu = False
+    else:
+        print('Opção inválida')
+    print('#')
+
+
+
+
 
