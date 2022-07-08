c6f3e26a7ad7dc229dae63058e6cbebf53c223fd
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-08 17:47:51 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#
+# Autores:
+# Michel Silva
+# Emanuel Frank
+# Carlos Eduardo
+# data: 08/07/2022
+#
+#############################################################################################################################
+print('#############################################')
+print("#            Exercício 1                    #")
+print("#############################################")
+#
+# importar a classe Veiculo
+from veiculo import Veiculo as V # importar a classe Veiculo como V
+
+carro1 = V('ABC1234', 'Fusca', 'Vermelho', 2)
+carro2 = V('XYZ1234', 'Gol', 'Azul', 4)
+
+print(carro1)
+print(carro2)
